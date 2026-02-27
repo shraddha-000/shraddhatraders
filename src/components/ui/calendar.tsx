d374f63364966197b515ae704e6e9590f2b7de6b
shraddha traders <shraddhatraders000@gmail.com>
2026-02-27 04:25:50 +0000

    PS D:\shraddhatraders-main> npm run build
    > nextn@0.1.0 build
    > next bu

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react"
 import { DayPicker } from "react-day-picker"
-import { ChevronLeft, ChevronRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -50,10 +49,6 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
-      }}
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
