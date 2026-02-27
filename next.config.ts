48f067e7094edc7e896d01bb7a5095d8578be443
shraddha traders <shraddhatraders000@gmail.com>
2026-02-27 03:39:39 +0000

    PS D:\shraddhatraders-main> npm run build
    > nextn@0.1.0 build
    > next bu

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    allowedDevOrigins: [
-        'https://*.cloudworkstations.dev',
-    ],
-  },
+  allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
